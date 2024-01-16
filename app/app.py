15197a25242eb68ba21ece0d0e5070e6be127010
adhishthite <adhish.thite@gmail.com>
2024-01-16 12:00:46 +0530

    added gunicorn+nginx

--- app/app.py
+++ app/app.py
@@ -1,22 +1,29 @@
+import os
+
 from flask import Flask
 import elasticapm
 from elasticapm.contrib.flask import ElasticAPM
+from dotenv import load_dotenv
+
+from . import data
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['ELASTIC_APM'] = {
-  'SERVICE_NAME': '',
-  'SECRET_TOKEN': '',
-  'SERVER_URL': '',
-  'ENVIRONMENT': '',
+  'SERVICE_NAME': os.environ['SERVICE_NAME'],
+  'SECRET_TOKEN': os.environ['SECRET_TOKEN'],
+  'SERVER_URL': os.environ['SERVER_URL'],
+  'ENVIRONMENT': os.environ['ENVIRONMENT'],
 }
 
 apm = ElasticAPM(app)
 
 
-@app.route('/get')
+@app.route('/')
 @elasticapm.capture_span()
 def hello_world():
-    return b"Hello World!"
+    return data.return_check()
 
 
 if __name__ == '__main__':
